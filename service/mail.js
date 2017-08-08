657dd0ad15f57eda09a1157e6df8478cbbe66325
fatefinger <532357944@qq.com>
2017-08-08 13:50:31 +0800

    优化mail.js

--- service/mail.js
+++ service/mail.js
@@ -16,13 +16,28 @@ const mailClass = function () {
             conf.AUTH_PWD
         }
     }
-    let mailTransport = nodemailer.createTransport(hostOptions)
+    // 获取主机配置
+    const getHostOptions = ()=>{
+        return hostOptions
+    }
+    // 邮件传输初始化
+    const mailTransport = nodemailer.createTransport(getHostOptions())
+
     // 图片附件列表
     let fileList = []
+    // set fileList
+    const setFileList = (value) => {
+        return new Promise((resolve, reject) => {
+            fileList = value
+            resolve(fileList)
+        })
+    }
+
     // 模板
     let htmlTemplate = ''
     // 模板存档
     let templateSaved = ''
+
     // 邮件内容选项
     const options = {
         from: conf.MAIL_FROM,
@@ -34,13 +49,27 @@ const mailClass = function () {
         html: '<h1>你好，这是一封来自my-mailer的邮件！</h1>',
         attachments: []
     }
-    // fileList赋值
-    const setFileList = (value) => {
-        return new Promise((resolve, reject) => {
-            fileList = value
-            resolve(fileList)
-        })
+     // get Options
+    const getOptions = () =>{
+        return options
     }
+    // get options.html
+    const getOptionsHtml = () =>{
+        return options.html
+    }
+    // set options.html
+    const setOptionsHtml = (value) => {
+        return options.html += value
+    }
+    // push item options.attachments
+    const pushOptionsAttachments = (item) => {
+        return options.attachments.push(item)
+    }
+    // setOptionsAttachments
+    const setOptionsAttachments = (value) =>{
+        return options.attachments = value
+    }
+
     // 创建html图片内容模板
     const createHtmlTemplate = (i) => {
         return '<img src="cid:0000000' + i + '"/>'
@@ -71,39 +100,39 @@ const mailClass = function () {
         let i = 0
         while (arr[i]) {
             i++
-            options.html += createHtmlTemplate(i)
-            options.attachments.push(createAttachmentList(i, fileList, conf.IMAGE_PATH))
+            setOptionsHtml(createHtmlTemplate(i))
+            pushOptionsAttachments(createAttachmentList(i, fileList, conf.IMAGE_PATH))
         }
     }
     // 暂存html模板
     const saveTemplate = () => {
-        templateSaved = options.html
-        htmlTemplate = options.html
+        templateSaved = getOptionsHtml()
+        htmlTemplate = getOptionsHtml()
     }
     // 发送邮件
     const sendMail = () => {
-        mailTransport.sendMail(options, function (err, msg) {
+        mailTransport.sendMail(getOptions(), function (err, msg) {
             if (err) {
-                console.log(err);
+                console.log(err)
             }
             else {
-                console.log(msg);
+                console.log(msg)
                 //邮件内容与附件列表还原
-                options.attachments = []
-                options.html = templateSaved
+                setOptionsAttachments([])
+                setOptionsHtml(templateSaved)
             }
         })
     }
     return {
         timingSend: () => {
             const rule = new schedule.RecurrenceRule()
-            conf.MODE === 'test'?(rule.second = conf.SECOND_TO_SEND):(rule.hour = conf.HOUR_TO_SEND)
+            conf.MODE === 'test' ? (rule.second = conf.SECOND_TO_SEND) : (rule.hour = conf.HOUR_TO_SEND)
             schedule.scheduleJob(rule, function () {
                 saveTemplate()
                 readdir(conf.IMAGE_PATH)
                     .then(_ => setFileList(_))
                     .then(_ => insertImage(_))
-                    .then(()=>sendMail())
+                    .then(() => sendMail())
                     .then(() => {
                         console.log('邮件发送成功')
                     })
